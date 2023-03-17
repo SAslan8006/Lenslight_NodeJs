9d97cebbd1ff76c62cc4f539198fc01e8517f074
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-03-17 15:52:04 +0300

    NODEJS Proje 03 -Veritabanı Bağlantısı, MongoDB Atlas,

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import conn from './db.js';
+import pageRoute from './routes/pageRoute.js';
+
+dotenv.config();
+
+//connection to the DB
+conn();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 //ejs template engine
 app.set('view engine', 'ejs');
@@ -9,13 +17,8 @@ app.set('view engine', 'ejs');
 //static files middleware
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
+//routes
+app.use('/', pageRoute);
 
 app.listen(port, () => {
   console.log(`Application running on port: ${port}`);
