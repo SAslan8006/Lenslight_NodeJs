2490e1ab8393832ebf3d57d8f4bc6b65fe7fafbf
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-03-17 18:42:14 +0300

    NODEJS Proje 06: Thunder Client Kullanımı, Document - Collection, Request - Response

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,9 +1,9 @@
 const getIndexPage = (req, res) => {
-    res.render('index');
-  };
-  
-  const getAboutPage = (req, res) => {
-    res.render('about');
-  };
-  
-  export { getIndexPage, getAboutPage };
+  res.render('index');
+};
+
+const getAboutPage = (req, res) => {
+  res.render('about');
+};
+
+export { getIndexPage, getAboutPage };
