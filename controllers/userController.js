e57500545f40e0263ced9b539ee9c832889cfccb
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-03-19 12:13:30 +0300

    NODEJS Proje 19: Takipçi - Takip Edilen II

--- controllers/userController.js
+++ controllers/userController.js
@@ -77,6 +77,7 @@ const getDashboardPage = async (req, res) => {
   res.render('dashboard', {
     link: 'dashboard',
     photos,
+    user,
   });
 };
 
@@ -98,11 +99,17 @@ const getAllUsers = async (req, res) => {
 const getAUser = async (req, res) => {
   try {
     const user = await User.findById({ _id: req.params.id });
+
+    const inFollowers = user.followers.some((follower) => {
+      return follower.equals(res.locals.user._id);
+    });
+
     const photos = await Photo.find({ user: user._id });
     res.status(200).render('user', {
       user,
       photos,
       link: 'users',
+      inFollowers,
     });
   } catch (error) {
     res.status(500).json({
@@ -126,15 +133,12 @@ const follow = async (req, res) => {
     user = await User.findByIdAndUpdate(
       { _id: res.locals.user._id },
       {
-        $push: { following: req.params.id },
+        $push: { followings: req.params.id },
       },
       { new: true }
     );
 
-    res.status(200).json({
-      succeded: true,
-      user,
-    });
+    res.status(200).redirect(`/users/${req.params.id}`);
   } catch (error) {
     res.status(500).json({
       succeded: false,
@@ -157,15 +161,12 @@ const unfollow = async (req, res) => {
     user = await User.findByIdAndUpdate(
       { _id: res.locals.user._id },
       {
-        $pull: { following: req.params.id },
+        $pull: { followings: req.params.id },
       },
       { new: true }
     );
 
-    res.status(200).json({
-      succeded: true,
-      user,
-    });
+    res.status(200).redirect(`/users/${req.params.id}`);
   } catch (error) {
     res.status(500).json({
       succeded: false,
